58f5221a2297076bf275468c488747ec6815cec1
hardik6869 <sorthiyahardik6869@gmail.com>
2023-03-13 13:32:01 +0530

    Add Sub Menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 const url = require("url");
 const path = require("path");
 
@@ -17,4 +17,28 @@ app.on("ready", function () {
       slashes: true,
     })
   );
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Create menu Template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+      },
+      {
+        label: "Clear Item",
+      },
+      {
+        label: "Quit",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
